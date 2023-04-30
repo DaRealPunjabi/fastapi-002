57e0ea21dac7eebe3451c93b880258d65a9c5275
DaRealPunjabi <shiv.jalli@gmail.com>
2023-04-30 17:55:21 +0100

    Create schemas.py
    Pydantic Models Deep Dive

--- app/main.py
+++ app/main.py
@@ -13,13 +13,12 @@ from psycopg2.extras import RealDictCursor
 import time
 from sqlalchemy.orm import Session
 
-from . import models
+from . import models, schemas
 from .database import engine, get_db
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
 
 class Post(BaseModel):
     title: str
@@ -49,25 +48,9 @@ while True:
 my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1},
             {"title": "favorite foods", "content": "I like pizza", "id": 2}]    
 
-def find_post(id):
-    for p in my_posts:
-        if p["id"] == id:
-            return p
-
-def find_index_post(id):
-    for  i, p in enumerate(my_posts):
-        if p['id'] == id:
-            return i
-
 @app.get("/")
 def root():
     return {"message": "Hello World ..."}
-
-@app.get("/sqlalchemy")
-def sqlalchemy(db: Session = Depends(get_db)):
-    posts = db.query(models.Post).all()
-    print(posts)
-    return {"data": posts}
 
 @app.get("/posts/{id}")
 #def get_post(id: int, response: Response):
@@ -93,7 +76,7 @@ def delete_post(id: int, db: Session = Depends(get_db)):
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @app.put("/posts/{id}")
-def update_post(id: int, post: Post,  db: Session = Depends(get_db)):
+def update_post(id: int, post: schemas.PostCreate,  db: Session = Depends(get_db)):
     print("In Update")
     print(id)
     print(post)
@@ -116,7 +99,7 @@ def get_posts(db: Session = Depends(get_db)):
     return {"data": posts}
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_posts(post: Post, db: Session = Depends(get_db)):
+def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
     # new_post = models.Post(title=post.title, content=post.content, published=post.published)
     new_post = models.Post(**post.dict())
     db.add(new_post)
