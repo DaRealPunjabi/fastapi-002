25163751735eb07a3d842774e1317ffff84563df
DaRealPunjabi <shiv.jalli@gmail.com>
2023-05-01 11:12:34 +0100

    Joins in SqlAlchemy

--- app/routers/post.py
+++ app/routers/post.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
 from sqlalchemy.orm import Session
+from sqlalchemy import func
 from typing import List, Optional
 from .. import models, schemas, utils, oauth2
 from ..database import get_db
@@ -9,13 +10,16 @@ router = APIRouter(
     tags=['Posts']
 )
 
-@router.get("/{id}", response_model=schemas.Post)
+@router.get("/{id}", response_model=schemas.PostOut)
 #def get_post(id: int, response: Response):
 def get_post(id: int, db: Session = Depends(get_db),
                  current_user: int = Depends(oauth2.get_current_user)):
     print(id)
     print(current_user.__dict__)
-    post = db.query(models.Post).filter(models.Post.id == id).first()
+    # post = db.query(models.Post).filter(models.Post.id == id).first()
+
+    post = db.query(models.Post, func.count(models.Vote.post_id).label("votes")).join(
+        models.Vote, models.Vote.post_id == models.Post.id, isouter=True).group_by(models.Post.id).filter(models.Post.id == id).first()
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id: {id} was not found")
@@ -68,14 +72,14 @@ def update_post(id: int, post: schemas.PostCreate,  db: Session = Depends(get_db
     print(updated_post.__dict__)
     return updated_post
 
-@router.get("/", response_model=List[schemas.Post])
+@router.get("/", response_model=List[schemas.PostOut])
 def get_posts(db: Session = Depends(get_db),
                  current_user: int = Depends(oauth2.get_current_user),
                  limit: int = 10, skip: int = 0, search: Optional[str] = ""):
     print("In Get")
     print(limit)
-    posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(
-        limit).offset(skip).all()
+    # posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(
+    #     limit).offset(skip).all()
     # posts = db.query(models.Post).limit(limit).all()
     # posts = db.query(models.Post).all()
     # posts = db.query(models.Post).filter(
@@ -83,6 +87,12 @@ def get_posts(db: Session = Depends(get_db),
     # print(type(posts))
     # for item in posts:
     #     print(item.__dict__)
+
+    posts = db.query(models.Post, func.count(models.Vote.post_id).label("votes")).join(
+        models.Vote, models.Vote.post_id == models.Post.id, isouter=True).group_by(
+        models.Post.id).filter(models.Post.title.contains(search)).limit(
+        limit).offset(skip).all()
+
     return posts
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
